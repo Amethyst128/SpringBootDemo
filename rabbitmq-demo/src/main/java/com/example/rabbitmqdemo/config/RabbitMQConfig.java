6052dd5439bbd1e33b1a0f4d09b110d452fed2a4
zy <1198809383@qq.com>
2019-09-17 17:17:24 +0800

    redis和线程池相关

---
+++ rabbitmq-demo/src/main/java/com/example/rabbitmqdemo/config/RabbitMQConfig.java
@@ -0,0 +1,14 @@
+package com.example.rabbitmqdemo.config;
+
+/**
+ * @Desc: MQ相关配置
+ * @Author: zy
+ * @Date:Created in 2019/8/12
+ */
+public class RabbitMQConfig {
+
+
+
+
+
+}
---
+++ rabbitmq-demo/src/main/java/com/example/rabbitmqdemo/config/RabbitMQConstant.java
@@ -0,0 +1,25 @@
+package com.example.rabbitmqdemo.config;
+
+/**
+ * @Desc: rabbitMQ 的相关定义
+ * @Author: zy
+ * @Date:Created in 2019/8/12
+ */
+public class RabbitMQConstant {
+
+    /**
+     * 消息交换机
+     */
+    public static final String QUEUE_MESSAGE = "message_queue";
+
+    /**
+     * 队列
+     */
+    public static final String QUEUE_MESSAGE_SEND_A = "message_queue_send_A";
+    public static final String QUEUE_MESSAGE_SEND_B = "message_queue_send_B";
+    public static final String QUEUE_MESSAGE_SEND_C = "message_queue_send_C";
+
+
+
+
+}
